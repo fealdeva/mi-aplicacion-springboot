373c6b4f934c63912ae88581fb63b3901972fa99
Felipe <fealdeva@gmail.com>
2025-02-28 13:23:44 -0600

    Se detalló en el mensaje HOLA MUNDO que es una investigación para documentar el cambio

--- src/main/java/com/investigacion/hellospringboot/controller/holaController.java
+++ src/main/java/com/investigacion/hellospringboot/controller/holaController.java
@@ -10,7 +10,7 @@ public class holaController {
 
     @GetMapping("/")
     public String hola(Model model) {
-        model.addAttribute("mensaje", "Hola Mundo desde SpringBoot en la investigación Felipe Delgado Valverde");
+        model.addAttribute("mensaje", "HOLA MUNDO DESDE SPRING BOOT! Investigación creada por Felipe Delgado Valverde como evidencia.");
         return "holamundo";
     }
     
